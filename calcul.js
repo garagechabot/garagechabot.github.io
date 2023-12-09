d71ba496d1e7342dee34a2e38f2f8dcdf2a94b09
Carine Croteau <carine.croteau@gmail.com>
2023-12-09 08:28:31 -0500

    changement des input types

--- calcul.js
+++ calcul.js
@@ -6,10 +6,10 @@ function updateTime(e) {
     console.log("mise à jour du temps");
     console.log(e);
     let tr = e.target.parentNode.parentNode;
-    let heureDebut = Number(tr.querySelector(".debut").value.split(":")[0]);
-    let minutesDebut = Number(tr.querySelector(".debut").value.split(":")[1]);
-    let heureFin = Number(tr.querySelector(".fin").value.split(":")[0]);
-    let minutesFin = Number(tr.querySelector(".fin").value.split(":")[1]);
+    let heureDebut = Number(tr.querySelector(".heureDebut").value);
+    let minutesDebut = Number(tr.querySelector(".minutesDebut").value);
+    let heureFin = Number(tr.querySelector(".heureFin").value);
+    let minutesFin = Number(tr.querySelector(".minutesFin").value);
     let hhmm = tr.querySelector(".hhmm");
     let hh100e = tr.querySelector(".hh100e");
 
@@ -32,15 +32,18 @@ function updateTime(e) {
     //console.log(fin)
 
     dureeMS = fin-debut; // durée en millisecondes
+    console.log(dureeMS);
 
     if(fin >= debut) {
         let minutes = Math.floor((dureeMS / (1000 * 60)) % 60);
         let hours = Math.floor((dureeMS / (1000 * 60 * 60)) % 24);
+        let minutes100e = Math.floor(minutes / 60 * 100);
       
         minutes = (minutes < 10) ? "0" + minutes : minutes;
+        minutes100e = (minutes100e < 10) ? "0" + minutes100e : minutes100e;
 
         hhmm.innerText = hours + ":" + minutes;
-        hh100e.innerText = hours + "." + Math.floor(minutes / 60 * 100);   
+        hh100e.innerText = hours + "." + minutes100e;   
     } else {
         hhmm.innerText = "-";
         hh100e.innerText = "-";
@@ -56,10 +59,10 @@ function updateTotal() {
     let trs = document.querySelectorAll("tr.timer");
     dureeTotale = 0;
     trs.forEach(tr => {
-        let heureDebut = Number(tr.querySelector(".debut").value.split(":")[0]);
-        let minutesDebut = Number(tr.querySelector(".debut").value.split(":")[1]);
-        let heureFin = Number(tr.querySelector(".fin").value.split(":")[0]);
-        let minutesFin = Number(tr.querySelector(".fin").value.split(":")[1]);
+        let heureDebut = Number(tr.querySelector(".heureDebut").value);
+        let minutesDebut = Number(tr.querySelector(".minutesDebut").value);
+        let heureFin = Number(tr.querySelector(".heureFin").value);
+        let minutesFin = Number(tr.querySelector(".minutesFin").value);
 
         // calculer le temps
         debut = new Date(2000, 1, 1);
@@ -75,17 +78,21 @@ function updateTotal() {
         dureeMS = fin-debut; // durée en millisecondes
         dureeTotale += dureeMS;
         }); 
-    if(dureeTotale >- 0) {
+    if(dureeTotale >= 0) {
         let minutes = Math.floor((dureeTotale / (1000 * 60)) % 60);
         let hours = Math.floor((dureeTotale / (1000 * 60 * 60)) % 24);
+        let minutes100e = Math.floor(minutes / 60 * 100);
       
         minutes = (minutes < 10) ? "0" + minutes : minutes;
+        minutes100e = (minutes100e < 10) ? "0" + minutes100e : minutes100e;
 
         dureeTotalehhmm.innerText = hours + ":" + minutes;
-        dureeTotalehh100e.innerText = hours + "." + Math.floor(minutes / 60 * 100);  
+        dureeTotalehh100e.innerText = hours + "." + minutes100e;  
     } else {
-        dureeTotalehhmm.innerText = "-";
-        dureeTotalehh100e.innerText = "-";
+        console.log(dureeTotalehhmm);
+        console.log(dureeTotalehh100e);
+        dureeTotalehhmm.innerText = "0:00";
+        dureeTotalehh100e.innerText = "0:00";
     }
 }
 
